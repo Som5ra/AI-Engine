56c3c19b9fb29a6b19230b9ff967be3163771df7
Sombraa711 <sombra.li@thisisgusto.com>
2024-10-23 16:57:44 +0800

    add install option and macos openmp option

---
+++ build/build-macos/src/CMakeFiles/GustoEngine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GustoEngine.dir/detector2d.cpp.o"
+  "CMakeFiles/GustoEngine.dir/detector2d.cpp.o.d"
+  "CMakeFiles/GustoEngine.dir/utils.cpp.o"
+  "CMakeFiles/GustoEngine.dir/utils.cpp.o.d"
+  "libGustoEngine.dylib"
+  "libGustoEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GustoEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
