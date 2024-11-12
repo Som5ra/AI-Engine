20a4a5d76cf73c8f9ca0403cb0cf4678862264df
Sombraa711 <sombra.li@thisisgusto.com>
2024-11-12 18:02:21 +0800

    update make config

--- src/BaseONNX.cpp
+++ src/BaseONNX.cpp
@@ -10,10 +10,10 @@ BaseONNX::BaseONNX(const std::string& model_path, const std::string& model_name)
     // session_options.SetIntraOpNumThreads(1);
     // session_options.SetInterOpNumThreads(1); 
     ort_session = Ort::Session(ort_env, model_path.c_str(), session_options);
-
+    std::cout << "Model Loaded: " << model_path << std::endl;
     size_t input_count = ort_session.GetInputCount();
     size_t output_count = ort_session.GetOutputCount();
-
+    std::cout << "Number of inputs: " << input_count << std::endl;
     for (size_t i = 0; i < input_count; ++i) {
         Ort::AllocatedStringPtr input_name_ptr = ort_session.GetInputNameAllocated(i, ort_allocator);
         const char* input_name = strdup(input_name_ptr.get());
@@ -26,6 +26,7 @@ BaseONNX::BaseONNX(const std::string& model_path, const std::string& model_name)
         }
         input_shape.push_back(dims);
     }
+    std::cout << "Number of outputs: " << output_count << std::endl;
     for (size_t i = 0; i < output_count; ++i) {
         Ort::AllocatedStringPtr output_name_ptr = ort_session.GetOutputNameAllocated(i, ort_allocator);
         const char* output_name = strdup(output_name_ptr.get());
